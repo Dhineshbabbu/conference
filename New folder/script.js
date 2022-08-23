3d708f32379a597ad91a29811a39c4af00f1ba96
Dhinesh__offi <85879973+Dhineshbabbu@users.noreply.github.com>
2022-08-23 09:11:18 +0530

    Add files via upload

---
+++ New folder/script.js
@@ -0,0 +1,50 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+
+toggleBtn.onclick = () =>{
+  themeToggler.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  themeToggler.classList.remove('active');
+}
+
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+  
+  btn.onclick = () =>{
+    let color = btn.style.background;
+    document.querySelector(':root').style.setProperty('--main-color', color);
+  }
+
+});
+
+var swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 10,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  loop:false,
+  autoplay:{
+    delay: 2000,
+    disableOnInteraction:false,
+  }
+});
+
+
